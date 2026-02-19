90f3280ad0f698bfb7dc6ca10343fcb98080aba4
Nam D. <namddanh@gmail.com>
2026-02-19 15:50:00 -0500

    :bug: lint error fixed useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -5,30 +5,29 @@ const useFetch = <T>(url: string) => {
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const fetchData = async () => {
-    setLoading(true);
-    setError(null);
-
-    try {
-      const res = await fetch(url);
-      if (!res.ok) throw new Error(`API fetch failed. ${res.status}`);
-
-      const data = await res.json();
-      setData(data);
-    } catch (err) {
-      const message =
-        err instanceof Error ? `${err.message}` : "Unknown error.";
-      setError(message);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const res = await fetch(url);
+        if (!res.ok) throw new Error(`API fetch failed. ${res.status}`);
+
+        const data = await res.json();
+        setData(data);
+      } catch (err) {
+        const message = err instanceof Error ? err.message : "Unknown error.";
+        setError(message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchData();
   }, [url]);
 
-  return { data, error, loading, refetch: fetchData };
+  return { data, error, loading };
 };
 
 export default useFetch;
